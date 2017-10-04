2fa870e941a077172ee17eb5fa3467ebc10bb051
regbake <regbake@gmail.com>
2017-10-04 14:16:28 -0700

    working on posting the text from user, routes are connected

--- auth-boilerplate/controllers/user.js
+++ auth-boilerplate/controllers/user.js
@@ -4,10 +4,31 @@ var db = require('../models');
 var passport = require('../config/ppConfig');
 var isLoggedIn = require('../middleware/isLoggedIn');
 
-router.get("/", isLoggedIn, function(req, res){
+//multer shiz
+var multer = require("multer");
+var fs = require("fs");
+var storage = multer.memoryStorage()
+var upload = multer({storage: storage});
+
+//removed isLoggedIn for testing
+router.get("/", function(req, res){
     res.render("user/home");
 });
 
+//handle the uploaded files
+router.post("/upload", upload.single("myFile"), function(req, res){
+  var text = req.file.buffer.toString("utf8");
+  userUploads.push(text);
+  console.log(userUploads);
+  // console.log(text, text.length);
+  res.redirect("/");
+});
+
+//to handle the uploaded text
+router.post("/", function(req, res){
+  console.log("text was submitted")
+  res.redirect("/user");
+});
 
 
 
