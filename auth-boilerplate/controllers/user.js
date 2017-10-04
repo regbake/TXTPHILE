8df8ca2e7c46f38fa9ba6d2c5ab49f7e053e92ce
regbake <regbake@gmail.com>
2017-10-04 13:55:02 -0700

    user authentication to get into the user page

--- auth-boilerplate/controllers/user.js
+++ auth-boilerplate/controllers/user.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models');
 var passport = require('../config/ppConfig');
+var isLoggedIn = require('../middleware/isLoggedIn');
 
-router.get("/", function(req, res){
+router.get("/", isLoggedIn, function(req, res){
     res.render("user/home");
 });
 
