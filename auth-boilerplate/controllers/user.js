b283cd50ba327e85ae4acc7336ba6d776b237eed
regbake <regbake@gmail.com>
2017-10-04 17:38:48 -0700

    fixed bug with the route collision, you can upload text or a file without problem

--- auth-boilerplate/controllers/user.js
+++ auth-boilerplate/controllers/user.js
@@ -12,37 +12,47 @@ var upload = multer({storage: storage});
 
 //removed isLoggedIn for testing
 router.get("/", isLoggedIn, function(req, res){
-    res.render("user/home", {currentUser: req.user});
+  var currentUserId = req.user.dataValues.id;
+
+  db.document.findAll({
+    where: {userId: currentUserId}
+  }).then(function(document){
+    res.render("user/home", {
+      currentUser: req.user,
+      document: document
+    });
+  });
 });
 
 //handle the uploaded files
 router.post("/", isLoggedIn, upload.single("myFile"), function(req, res){
-  var text = req.file.buffer.toString("utf8");
   var currentUserId = req.user.dataValues.id;
-
-  db.document.create({
-    body: text,
-    userId: currentUserId
-  })
-  console.log(text);
-  res.redirect("/user");
-});
-
-//to handle the uploaded text
-router.post("/", isLoggedIn, function(req, res){
   var userInput = req.body.userInput; //input from textform
-  var currentUserId = req.user.dataValues.id;
 
-  db.document.create({
-    body: userInput,
-    userId: currentUserId
-  }).then(function(){
+  if (req.file !== undefined){
+    var text = req.file.buffer.toString("utf8"); //text from upload
+  }
+
+  if (text !== undefined) { //the input from textform
+    
+    db.document.create({
+      body: text,
+      userId: currentUserId
+    })
     res.redirect("/user");
-  });
+  } else if (userInput !== undefined) {
 
-  //user upload is in a string, I should be able to put this into the
-  //db at this point, I can add functions to clean the data later
+    db.document.create({ //to handle the uploaded text
+      body: userInput,
+      userId: currentUserId
+    }).then(function(){
+      res.redirect("/user");
+    });
+  } else {
+    res.redirect("/user");
+  }
 });
+
 
 
 
